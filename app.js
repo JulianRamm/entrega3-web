ef9f27d9d33706eadcb85b06bd797953960d7c1d
je.osorio <juliyanes530@gmail.com>
2020-10-27 00:42:13 -0500

    ultimo commit

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ var donationsRouter = require("./routes/donations");
 var reportsRouter = require("./routes/reports");
 
 var app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.use(logger("dev"));
 app.use(express.json());
